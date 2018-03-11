f3910a1ab23cbe6542dfa574d6180aa58d1c2b42
youngzer <youngzer@qq.com>
2018-03-11 20:49:19 +0800

    108_杨晨朝 第1课作业

---
+++ Lesson-1/assignment/payroll.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.4.14;
+
+contract Payroll {
+    uint constant payDuration = 10 seconds;
+
+    address owner;
+    uint salary;
+    address employee;
+    uint lastPayday;
+
+    function Payroll() {
+        owner = msg.sender;
+    }
+
+    function addFund() payable returns (uint) {
+        return this.balance;
+    }
+
+    function setEmployeeAddressSalary(address newAddress, uint newSalary) {
+        require(msg.sender == owner);        
+
+        employee = newAddress;
+        salary = newSalary * 1 ether;
+    }
+
+    function calculateRunway() returns (uint) {
+        return this.balance / salary;
+    }
+    
+    function hasEnoughFund() returns (bool) {
+        return calculateRunway() > 0;
+    }
+    
+    function getPaid() {
+        require(msg.sender == employee);
+        
+        uint nextPayday = lastPayday + payDuration;
+        assert(nextPayday < now);
+
+        lastPayday = nextPayday;
+        employee.transfer(salary);
+    } 
+}
