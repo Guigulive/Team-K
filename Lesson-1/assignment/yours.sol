89b83ff9ecd03b707a0eeb8774928e49d4caa961
yizhen <jonkiky@gmail.com>
2018-03-11 11:03:10 -0400

    1's assignment

--- Lesson-1/assignment/yours.sol
+++ Lesson-1/assignment/yours.sol
@@ -1,1 +1,52 @@
-/*作业请提交在这个目录下*/
+pragma solidity ^0.4.14;
+
+contract Payroll {
+    
+    uint salary ;
+    address frank ;
+    uint constant payrollDuraton = 10 seconds;
+    uint lastPayDay = now;
+    
+    function setAddress(){
+        frank = msg.sender;
+    }
+
+     function setAddress(address newAddress){
+        frank = newAddress;
+    }
+    
+    function getAddress() returns(address){
+        return frank;
+    }
+     
+    function setSalary(uint money){
+        salary = money;
+    }
+    
+    function getSalary() returns(uint){
+        return salary;
+    }
+    function addFound() payable returns(uint){
+        return this.balance;
+    }
+    
+    function calculateRunWay() returns(uint){
+        return this.balance / salary;
+    }
+    
+    function hasEnoughFound() returns(bool){
+        return calculateRunWay() > 0;
+    }
+    
+    function getPaid(){
+        uint nextPayDay = lastPayDay +payrollDuraton ;
+        if(nextPayDay>now){
+            revert();
+        }
+        
+        lastPayDay = nextPayDay;
+        frank.transfer(salary);
+    }
+    
+    
+}
