899e63053d099197d03a9a6d1917c0abe0966449
金马 <lijinma@126.com>
2018-03-14 17:36:22 +0800

    Update yours.sol
    添加两个方法

--- Lesson-1/assignment/yours.sol
+++ Lesson-1/assignment/yours.sol
@@ -1,1 +1,52 @@
-/*作业请提交在这个目录下*/
+pragma solidity ^0.4.14;
+
+contract Payroll {
+    uint constant payDuration = 10 seconds;
+
+    address owner;
+    uint salary = 1 ether;
+    address employee = 0x14723a09acff6d2a60dcdf7aa4aff308fddc160c;
+    uint lastPayday;
+
+    function Payroll() {
+        owner = msg.sender;
+    }
+    
+    function updateEmployee (address e) {
+        require(msg.sender == owner);
+        
+        employee = e;
+        lastPayday = now;
+    }
+    
+    function updateSalary (uint s) {
+        require(msg.sender == owner);
+        
+        salary = s;
+    }
+    
+    
+    function addFund() payable returns (uint) {
+        return this.balance;
+    }
+    
+    function calculateRunway() returns (uint) {
+        return this.balance / salary;
+    }
+    
+    function hasEnoughFund() returns (bool) {
+        return calculateRunway() > 0;
+    }
+    
+    function getPaid() {
+        require(msg.sender == employee);
+        
+        uint nextPayDay = lastPayday + payDuration;
+        if (nextPayDay > now) {
+            revert();
+        }
+        
+        lastPayday = nextPayDay;
+        employee.transfer(salary);
+    }
+}
