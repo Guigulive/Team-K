f5b977dea5d1e8a4784bd9c0e97e27477cc93863
HongGuo Smith <jinrishouji@sina.com>
2018-03-17 16:43:17 +0800

    1 增加第二课的问题回答   2 优化第二课智能合约

--- Lesson-2/assignment/yours.sol
+++ Lesson-2/assignment/yours.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.4.14;
 
 contract Payroll {
     uint constant payDuration = 5 seconds;
+    uint totalSalary = 0;
     
     struct Employee {
         address id;
@@ -10,7 +11,7 @@ contract Payroll {
     }
     
     address owner;
-    Employee[] employees;
+    Employee[] public employees;
     
     event TransferSalary(uint salary);
     
@@ -18,14 +19,16 @@ contract Payroll {
         owner = msg.sender;
     }
     
+    //支付之前的部分
     function _partialPaid(Employee employee) private {
          uint payment = employee.salary * (now - employee.lastPayday) / payDuration;
-         if (payment > 0 && hasEnoughtFund() ) {
+         if (payment > 0) {
             employee.id.transfer(payment);
             TransferSalary(payment);
         }
     }
     
+    //寻找数组是否存在雇员地址
     function _findEmployee(address employee) private returns (Employee, uint) {
          for(uint i =0; i< employees.length; i++)
         {
@@ -42,7 +45,8 @@ contract Payroll {
         var ( employee, index) = _findEmployee(employeeId);
         assert(employee.id == 0x0);
         
-        employees.push(Employee(employee.id, salary, now));
+        employees.push(Employee(employeeId, salary  * 1 ether, now));
+        totalSalary += salary  * 1 ether;
     }
     
     function removeEmployee(address employeeId) {
@@ -52,6 +56,8 @@ contract Payroll {
         assert(employee.id != 0x0);
         
         _partialPaid(employee);
+        totalSalary -= employee.salary  * 1 ether;
+        
         delete employees[index];
         employees[index] = employees[employees.length -1];
         employees.length -=1;
@@ -64,8 +70,11 @@ contract Payroll {
         assert(employee.id != 0x0);
         
         _partialPaid(employee);
-        employees[index].salary = salary;
+        totalSalary-=employee.salary  * 1 ether;
+        
+        employees[index].salary = salary * 1 ether;
         employees[index].lastPayday = now;
+        totalSalary += employee.salary  * 1 ether;
 	}
     
     //1 给合约账户充值 2 查看合约余额
@@ -75,11 +84,7 @@ contract Payroll {
     
     //查看账户余额能发几次薪水
     function caculateRunway() returns (uint) {
-        uint totalSalary = 0;
-         for(uint i =0; i< employees.length; i++)
-        {
-            totalSalary += employees[i].salary;
-        }
+        assert(totalSalary != 0);
         return this.balance / totalSalary;
     }
     
@@ -101,7 +106,7 @@ contract Payroll {
             revert();
         }
         
-        if( 0x0 != employee.id &&  hasEnoughtFund() ) {
+        if( 0x0 != employee.id ) {
             employees[index].lastPayday = nextPayDay;
             employee.id.transfer(employee.salary);
             TransferSalary(employee.salary);
