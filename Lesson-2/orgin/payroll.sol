8078d1572442fb502d18569ab3fac812ad8b81fe
yizhen <jonkiky@gmail.com>
2018-03-23 23:05:13 -0400

    104 陈益振 第二课作业

--- Lesson-2/orgin/payroll.sol
+++ Lesson-2/orgin/payroll.sol
@@ -17,21 +17,54 @@ contract Payroll {
     }
     
     function _partialPaid(Employee employee) private {
+        uint payment = employee.salary * (now - employee.lastPayday) / payDuration;
+        employee.id.transfer(payment);
     }
     
     function _findEmployee(address employeeId) private returns (Employee, uint) {
+        for(uint i =0; i< employees.length; i++)
+            {
+                if(employees[i].id == employee)
+                {
+                    return (employees[i], i);
+                }
+            }
+        return (Employee(0,0,0), 0);
     }
 
     function addEmployee(address employeeId, uint salary) {
+        require(msg.sender == owner);
+        var (employee, index) = _findEmployee(employeeId);
+        assert(employee.id == 0x0); 
+        employees.push(Employee(employeeId, salary * 1 ether, now));
+        totalSalary += salary * 1 ether;
     }
     
     function removeEmployee(address employeeId) {
+        require(msg.sender == owner);
+        var (employee, index) = _findEmployee(employeeId);
+
+        assert(employee.id != 0x0); 
+        _partialPaid(employee);
+        totalSalary -= employee.salary;
+        delete employees[index];
+        employees[index] = employees[employees.length - 1];
+        employees.length -= 1;
     }
     
     function updateEmployee(address employeeId, uint salary) {
+        require(msg.sender == owner);    
+        var ( employee, index) = _findEmployee(employeeId);
+        assert(employee.id != 0x0);        
+        _partialPaid(employee);
+        totalSalary -= employee.salary;      
+        employees[index].salary = salary * 1 ether;
+        employees[index].lastPayday = now;
+        totalSalary += employees[index].salary;
     }
     
     function addFund() payable returns (uint) {
+        return this.balance;
     }
     
     function calculateRunway() returns (uint) {
@@ -43,8 +76,17 @@ contract Payroll {
     }
     
     function hasEnoughFund() returns (bool) {
+        return calculateRunway() > 0;
     }
     
     function getPaid() {
+
+        var (employee, index) = _findEmployee(msg.sender);
+        assert(employee.id != 0x0);
+        uint nextPayday = employee.lastPayday + payDuration;
+        assert(nextPayday < now);
+        uint payTimes = (now - employee.lastPayday) / payDuration;
+        employees[index].lastPayday = employee.lastPayday + payDuration * payTimes;
+        employee.id.transfer(employee.salary * payTimes);
     }
 }
