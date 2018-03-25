f9ea49fce3c0e6c89c1372245c82357c5ee0f6a8
youngzer <youngzer@qq.com>
2018-03-25 21:52:33 +0800

    解决Test removeEmployee用例的invalid opcode错误，addEmployee只需执行一次

--- Lesson-4/assignment/test/payroll.js
+++ Lesson-4/assignment/test/payroll.js
@@ -5,24 +5,24 @@ contract('Payroll', function(accounts) {
   it("Test addEmployee", function() {
       return Payroll.deployed().then(function(instance) {
         payroll = instance;
-        return payroll.addEmployee(accounts[2], 1);
+        return payroll.addFund({from: accounts[0], value: 100});
       }).then(function() {
-        return payroll.getEmployee.call(accounts[2]);
+        return payroll.addEmployee(accounts[1], 1);
+      }).then(function() {
+        return payroll.getEmployee(accounts[1]);
       }).then(function(employeeId) {
-        assert.equal(employeeId, accounts[2], "add employee wrong");
+        assert.equal(employeeId, accounts[1], "Add employee failed!");
       });
     });
 
   it("Test removeEmployee", function() {
     return Payroll.deployed().then(function(instance) {
         payroll = instance;
-        return payroll.addEmployee(accounts[2], 1);        
+        return payroll.removeEmployee(accounts[1]);
       }).then(function() {
-        return payroll.removeEmployee(accounts[2]);
-      }).then(function() {
-        return payroll.getEmployee.call(accounts[2]);
+        return payroll.getEmployee(accounts[1]);
       }).then(function(employeeId) {
-        assert.equal(employeeId, 0, "remove employee wrong");
+        assert.equal(employeeId, 0, "Remove employee failed!");
       });
     });
 });
