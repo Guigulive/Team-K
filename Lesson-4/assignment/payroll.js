ef6081a8e687a57a743e71a86f8a1f06b416a6f4
HongGuo Smith <jinrishouji@sina.com>
2018-03-27 16:33:11 +0800

    使用async/await优化代码

--- Lesson-4/assignment/payroll.js
+++ Lesson-4/assignment/payroll.js
@@ -1,50 +1,42 @@
 var Payroll = artifacts.require("./Payroll.sol");
 
 //contract这儿等同于mocha的describe
-contract('Payroll', function(accounts){   
-    it("addEmployee test ", function() {
-        return Payroll.deployed().then(function(instance) {
-            payrollInstance = instance;
-            payrollInstance.addFund({from: accounts[0], value: 1});
+contract('Payroll', function(accounts){
 
-            return payrollInstance.addEmployee(accounts[1], 1);
-        }).then(function() {
-            return payrollInstance.employees.call(accounts[1]);
-        }).then(function(employee){
-            console.log(employee);
-            assert.equal(employee[0], accounts[1], "The value " + accounts[1] + " was not stored.");
-        }).then(function() { 
-            assert(false, "exception is supposed to throw but didn't.");
-        }).catch(function(err) {
-            // TODO: can do more careful validation by comparing the invalid opcode
-            assert(true);
-        });
+    let payrollInstance;
+
+    before(async () => {
+        payrollInstance = await Payroll.new({from : accounts[0]});
+        const wei_added = web3.toWei("5", "ether");
+        await payrollInstance.addFund({from : accounts[0], value : wei_added});
+        console.log("run before");
     });
 
-    it("removeEmployee test ", function() {
-        return Payroll.deployed().then(function(instance) {
-            payrollInstance = instance;
-            payrollInstance.addFund({from: accounts[0], value: 1});
-
-            return payrollInstance.employees.call(accounts[1]);
-        }).then(function(employee){
-            //console.log(employee);
+    it("addEmployee test ", async () => {
+        try {
+            await payrollInstance.addEmployee(accounts[1], 1, {from: accounts[0]});
+            const employee = await payrollInstance.employees.call(accounts[1]);
             assert.equal(employee[0], accounts[1], "The value " + accounts[1] + " was not stored.");
-        }).then(function(){
-            payrollInstance.removeEmployee(accounts[1]);
-        }).then(function() {
-            return payrollInstance.employees.call(accounts[1]);
-        }).then(function(employee){
-            console.log(employee);
+        } catch(err) {
+            // TODO: can do more careful validation by comparing the invalid opcode
+            assert(true);
+        }
+    });
+
+    it("removeEmployee test ", async () => {
+        try {
+            const employeeExit = await payrollInstance.employees.call(accounts[1]);
+            assert.equal(employeeExit[0], accounts[1], "The value " + accounts[1] + " was not stored.");
+
+            await payrollInstance.removeEmployee(accounts[1]);
+
+            const employee = await payrollInstance.employees.call(accounts[1]);
             assert.equal(employee[0], 0, "The value " + employee[0] + " was not null.");
-        }).then(function() { 
-            assert(false, "exception is supposed to throw but didn't.");
-        }).catch(function(err) {
+
+        } catch(err) {
             // TODO: can do more careful validation by comparing the invalid opcode
             assert(true);
-        });
+        }
     });
-
-
 
 });
